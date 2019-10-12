4a2aec9d267d2c40ab47fcea8d787c7e21bf43da
Sandeep <sandiipkd@gmail.com>
2019-10-12 15:28:01 +0530

    pdf changed

--- src/app/Admin/App.Admin.MainComponenet.ts
+++ src/app/Admin/App.Admin.MainComponenet.ts
@@ -15,12 +15,18 @@ export class AdminMainComponent implements OnInit {
 //   @Input() user :  User;
 
 @ViewChild('content', {static: false}) content : ElementRef;
+@ViewChild('userText', {static: false}) userText : ElementRef;
   users : any;
 items : any;
 // imgSrc :any = null;
 selectedImage : any = null;
 downloadedUrls :any[];
- 
+r : any =0;
+s : any =0;
+t : any= 0 ;
+// imgSrc: any;
+imagList : any ;
+rowIndexArray : any [];
   constructor(private formService: FormService, config: NgbCarouselConfig) { 
     config.interval = 5000;  
     config.wrap = true;  
@@ -46,15 +52,14 @@ downloadedUrls :any[];
       this.items= this.users;
       for(let user of users){
       this.downloadedUrls= user.downloadURLs;
-      
+      console.log(this.downloadedUrls);
       }
       return this.downloadedUrls;
     });
   }
  
   downloadPDF(){
-    let doc = new jsPDF('p','pt','a4');
-
+    let doc = new jsPDF('p','px','a4');
   
     let specialElementHandlers = {
       '#editor' : function(element, renderer){
@@ -62,18 +67,86 @@ downloadedUrls :any[];
       } 
     };
   
+    // let userText = this.userText.nativeElement;
     let content = this.content.nativeElement;
-    doc.fromHTML(content.innerHTML,15, 15, {
-      'autoresize':"true",
-      'width' :190,
-      'elementHandlers': specialElementHandlers  
+    for (let j=0; j<this.users.length; j++){
+    // for(let user of this.users){
+     
+    this.downloadedUrls = this.users[j].downloadURLs;
+   // this.imagList = this.downloadedUrls.length;
+    // this.rowIndexArray = Array.from(Array(Math.ceil(this.downloadedUrls.length/3)).keys());
+    // console.log(this.rowIndexArray);
+    // for (let k = 0; k< this.rowIndexArray.length; k++){
+      doc
+    for(let i=0; i<this.downloadedUrls.length; i++){
+      const imgSrc =this.downloadedUrls[i];
+      const p = i%3;
+      // const imgAddress = this.getBase64Image(imgSrc);
+      const k = (Math.floor(i/3)+1);
+      console.log(k);
+      this.s =j;
+       this.r = this.s + 150*k;
+      const t = k*j
+      
+      this.convertToDataURLviaCanvas(imgSrc, "image/jpeg")
+    .then( base64Img => {
+    doc.addImage(base64Img, 'jpg', 100*p, 150*j*k,);
+    });
+    // doc.addImage(imgAddress, 'JPEG',220,220, );
+    
+  }
+    doc.fromHTML(content.innerHTML,150,150, {
+      'elementHandlers': specialElementHandlers
     },
-
     function(bla){doc.save('saveInCallback.pdf');},
-  0);
+    0);
+
+  // }
+  }
+  doc.save('saveInCallback.pdf');
+  // doc.addImage(imgAddress, 'JPEG',220,220, );
+      
+  //   doc.fromHTML(content.innerHTML,150, 150, {
+  //     'width' :590,
+  //     'elementHandlers': specialElementHandlers  
+  //   },
+
+  //   function(bla){doc.save('saveInCallback.pdf');},
+  // 0);
+
+
   }
 deleteAllCustomers() {
   this.formService.deleteAll().catch(err => console.log(err));
 }
+// getBase64Image(img) {
+//   const canvas = document.createElement("canvas");
+//   canvas.width = img.width;
+//   canvas.height = img.height;
+//   // const ctx = canvas.getContext("2d").drawImage(img, 0, 0);
+  
+//   const dataURL = canvas.toDataURL("image/png");
+//   return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
+// }
 
+convertToDataURLviaCanvas(url, outputFormat){
+	return new Promise( (resolve, reject) => {
+		let img = new Image();
+		img.crossOrigin = 'Anonymous';
+		img.onload = function(){
+			let canvas = <HTMLCanvasElement> document.createElement('CANVAS'),
+			ctx = canvas.getContext('2d'),
+			dataURL;
+			canvas.height = img.height;
+			canvas.width = img.width;
+			ctx.drawImage(img, 0, 0);
+			dataURL = canvas.toDataURL(outputFormat);
+			//callback(dataURL);
+			canvas = null;
+			resolve(dataURL); 
+		};
+		img.src = url;
+	});
 }
+
+} 
